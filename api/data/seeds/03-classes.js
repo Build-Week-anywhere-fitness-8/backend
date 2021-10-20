eaefd5531577544928b3fbef16b2f1cb51723880
George <cavazosgeorge@yahoo.com>
2021-10-20 18:56:54 -0400

    Update class seeds

--- api/data/seeds/03-classes.js
+++ api/data/seeds/03-classes.js
@@ -13,6 +13,38 @@ exports.seed = function (knex) {
           class_location: "New York",
           class_max: "10",
         },
+        {
+          class_name: "Soul Cycle",
+          class_type: "Cardio",
+          class_duration: "120",
+          class_intensity: "Advanced",
+          class_location: "Miami",
+          class_max: "10",
+        },
+        {
+          class_name: "Boxing",
+          class_type: "Cardio",
+          class_duration: "60",
+          class_intensity: "Intermediate",
+          class_location: "Grand Rapids",
+          class_max: "20",
+        },
+        {
+          class_name: "Hot Yoga",
+          class_type: "Yoga",
+          class_duration: "60",
+          class_intensity: "Beginner",
+          class_location: "Chicago",
+          class_max: "10",
+        },
+        {
+          class_name: "Cross-fit",
+          class_type: "Strength",
+          class_duration: "60",
+          class_intensity: "Advanced",
+          class_location: "Los Angeles",
+          class_max: "10",
+        },
       ]);
     });
 };
